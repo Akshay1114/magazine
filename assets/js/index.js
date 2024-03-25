99c51250b686c2e5dbf2c143b30f5a0a65f24ac8
Akshay1114 <sharma41114@gmail.com>
2024-03-24 18:04:31 -0700

    updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,119 +31,25 @@ getElementbyId("navBarToggle").addEventListener("click", function() {
 
 
 // CARASOULE
-const $menu = document.querySelector('.menu')
-const $items = document.querySelectorAll('.menu--item')
-const $images = document.querySelectorAll('.menu--item img')
-let menuWidth = $menu.clientWidth
-let itemWidth = $items[0].clientWidth
-let wrapWidth = $items.length * itemWidth
 
-let scrollSpeed = 0
-let oldScrollY = 0
-let scrollY = 0
-let y = 0
-
-
-/*--------------------
-Lerp
---------------------*/
-const lerp = (v0, v1, t) => {
-  return v0 * ( 1 - t ) + v1 * t
-}
-
-
-/*--------------------
-Dispose
---------------------*/
-const dispose = (scroll) => {
-  gsap.set($items, {
-    x: (i) => {
-      return i * itemWidth + scroll
-    },
-    modifiers: {
-      x: (x, target) => {
-        const s = gsap.utils.wrap(-itemWidth, wrapWidth - itemWidth, parseInt(x))
-        return `${s}px`
-      }
-    }
-  })
-} 
-dispose(0)
-
-
-/*--------------------
-Wheel
---------------------*/
-const handleMouseWheel = (e) => {
-  scrollY -= e.deltaY * 0.9
-}
-
-
-/*--------------------
-Touch
---------------------*/
-let touchStart = 0
-let touchX = 0
-let isDragging = false
-const handleTouchStart = (e) => {
-  touchStart = e.clientX || e.touches[0].clientX
-  isDragging = true
-  $menu.classList.add('is-dragging')
-}
-const handleTouchMove = (e) => {
-  if (!isDragging) return
-  touchX = e.clientX || e.touches[0].clientX
-  scrollY += (touchX - touchStart) * 2.5
-  touchStart = touchX
-}
-const handleTouchEnd = () => {
-  isDragging = false
-  $menu.classList.remove('is-dragging')
-}
-
-
-/*--------------------
-Listeners
---------------------*/
-$menu.addEventListener('mousewheel', handleMouseWheel)
-
-$menu.addEventListener('touchstart', handleTouchStart)
-$menu.addEventListener('touchmove', handleTouchMove)
-$menu.addEventListener('touchend', handleTouchEnd)
-
-$menu.addEventListener('mousedown', handleTouchStart)
-$menu.addEventListener('mousemove', handleTouchMove)
-$menu.addEventListener('mouseleave', handleTouchEnd)
-$menu.addEventListener('mouseup', handleTouchEnd)
-
-$menu.addEventListener('selectstart', () => { return false })
-
-
-/*--------------------
-Resize
---------------------*/
-window.addEventListener('resize', () => {
-  menuWidth = $menu.clientWidth
-  itemWidth = $items[0].clientWidth
-  wrapWidth = $items.length * itemWidth
-})
-
-
-/*--------------------
-Render
---------------------*/
-const render = () => {
-  requestAnimationFrame(render)
-  y = lerp(y, scrollY, .1)
-  dispose(y)
-  
-  scrollSpeed = y - oldScrollY
-  oldScrollY = y
-  
-  gsap.to($items, {
-    skewX: -scrollSpeed * .2,
-    rotate: scrollSpeed * .01,
-    scale: 1 - Math.min(100, Math.abs(scrollSpeed)) * 0.003
-  })
-}
-render()
+const buttons = document.querySelectorAll('[data-carasoule-btn]');
+// console.log(buttons);
+buttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+        const offset = btn.dataset.carasouleBtn == 'prev' ? -1 : 1;
+        const slides = btn.closest('[data-carasoule]').querySelectorAll('[data-slides]');
+    console.log("slides<<<<<<<<<<<<<<< ====>", slides);
+        // console.log("slides ====>", [...slides.children]);
+        
+        const activeSlides = slides.querySelector('[data-slides]>[data-slide]');
+        console.log("activeSlides ====>", activeSlides);
+        let activeIndex = [...slides.children].indexOf(activeSlides) + offset;
+        if (activeIndex < 0) {
+            activeIndex = slides.children.length - 1;
+        } else if (activeIndex >= slides.length) {
+            activeIndex = 0;
+        }
+        slides.children[activeIndex].dataset.active = true;
+        delete activeSlides.dataset.active;
+    });
+});
